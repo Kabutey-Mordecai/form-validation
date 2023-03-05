2d3414deacdcee86366f93b783b2cec205d1a17e
GhSlimi <kabuteymordecai@outlook.com>
2023-03-05 22:49:55 +0000

    javascript form validation

---
+++ script.js
@@ -0,0 +1,41 @@
+let id =(id) =>{
+   return document.getElementById(id)
+}
+let classes =(classes) =>{
+   return document.getElementsByClassName(classes)
+}
+
+
+let username =id('username'),
+ email = id('email'),
+ password = id('password'),
+ form =id("form"),
+ errorMsg =classes("error"),
+ successIcon =classes("success-icon"),
+ failureIcon = classes('failure-icon');
+
+
+ form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    engine(username,0, "Username cannot be blank");
+    engine(email,1, "Email cannot be blank");
+    engine(password,2, "Password cannot be blank");
+ })
+
+
+ let engine =(id, serial,messege)=>{
+    
+    if(id.value.trim()===''){
+        errorMsg[serial].innerHTML = messege;
+        failureIcon[serial].style.opacity='1'
+        successIcon[serial].style.opacity='0'
+    }
+
+    else{
+        errorMsg[serial].innerHTML = '';
+        successIcon[serial].style.opacity='1'
+        failureIcon[serial].style.opacity='0'
+    }
+
+ }
