a34bcb9c03a167e4c3769bb72723268293c10fb6
GhSlimi <kabuteymordecai@outlook.com>
2023-03-05 23:16:36 +0000

    m

--- script.js
+++ script.js
@@ -44,22 +44,4 @@ let username =id('username'),
 
 
 
- const handleSubmit = (event) => {
-   event.preventDefault();
- 
-   const myForm = event.target;
-   const formData = new FormData(myForm);
-   
-   fetch("/", {
-     method: "POST",
-     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-     body: new URLSearchParams(formData).toString(),
-   })
-     .then(() => console.log("Form successfully submitted"))
-     .catch((error) => alert(error));
- };
- 
- document
-   .querySelector("form")
-   .addEventListener("submit", handleSubmit);
  
